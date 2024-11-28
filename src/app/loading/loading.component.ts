940dff2641bfb6266204ceeb14230cd33641577d
MontyOtter <o.dacosta@outlook.com>
2024-11-28 13:18:58 +0100

    Refactor loading component to remove merge conflict artifacts and streamline navigation logic

--- src/app/loading/loading.component.ts
+++ src/app/loading/loading.component.ts
@@ -4,17 +4,14 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-loading',
   templateUrl: './loading.component.html',
-  styleUrls: ['./loading.component.css']
+  styleUrls: ['./loading.component.css'],
 })
 export class LoadingComponent implements OnInit, OnDestroy {
   fadeOut = false;
   isLoading: boolean = true;
 
   constructor(private router: Router) {}
-<<<<<<< HEAD
 
-=======
->>>>>>> de25069d0889a6b048c1db1d343bd58456eef6f7
   ngOnInit(): void {
     setTimeout(() => {
       this.isLoading = false;
@@ -24,13 +21,8 @@ export class LoadingComponent implements OnInit, OnDestroy {
 
   triggerFadeOut(): void {
     this.fadeOut = true;
-<<<<<<< HEAD
-=======
     this.router.navigate(['/home']);
->>>>>>> de25069d0889a6b048c1db1d343bd58456eef6f7
-    setTimeout(() => {
-      this.router.navigate(['/home']);
-    }, 1000);
+    this.router.navigate(['/home']);
   }
 
   ngOnDestroy(): void {
